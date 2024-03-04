de42a0b62d885b97d3f70c6e897b390ae6f18d6c
nomad9898 <abouzarabbaspour@gmail.com>
2024-03-04 21:40:19 +0100

    set angle

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import requests
 
 servo = AngularServo(18, min_angle=-90, max_angle=90)
 
-while True:
-    response = requests.get("https://api.example.com/data?id=10001") 
-    data = response.json()
-    value = data['value']
-    servo.angle = value
-    sleep(5)
+def set_angle(angle):
+    servo.angle = angle
+
+
+# while True:
+#     response = requests.get("https://api.example.com/data?id=10001") 
+#     data = response.json()
+#     value = data['value']
+#     servo.angle = value
+#     sleep(5)
