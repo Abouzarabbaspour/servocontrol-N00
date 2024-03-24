d67b1cadfd310fe394abc5de3829fc169aec161f
nomad9898 <abouzarabbaspour@gmail.com>
2024-03-24 15:24:19 +0100

    adding logs

--- startup_nomadiot_001.sh
+++ startup_nomadiot_001.sh
@@ -3,11 +3,15 @@
 cd "$(dirname "$0")"
 
 # Start the Bluetooth agent and configure Bluetooth settings
+echo "Starting Bluetooth agent..."
+echo "Configuring Bluetooth settings..."
 bluetoothctl system-alias nomadiot_001
+echo "Powering off Bluetooth..."
 bluetoothctl power off
+echo "Powering on Bluetooth..."
 bluetoothctl power on
+echo "Setting device to discoverable..."
 bluetoothctl discoverable on
-
 # Navigate to the specified directory
 cd /home/servocontrol-N00
 
