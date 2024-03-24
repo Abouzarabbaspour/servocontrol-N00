f99246429c594a12f71a49d48be96c13362973b7
nomad9898 <abouzarabbaspour@gmail.com>
2024-03-24 15:03:09 +0100

    read values from json

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-
+import json
 
 angle_values = {
     'open': 0,
     'close': -90
 }
+# read values from json file
+with open('values.json') as f:
+    data = json.load(f)
+    angle_values['open'] = data['open']
+    angle_values['close'] = data['close']
+    print(angle_values)
