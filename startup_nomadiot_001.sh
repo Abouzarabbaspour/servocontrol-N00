019c38cc691873d9eef1a82c76b72685f9a97c74
nomad9898 <abouzarabbaspour@gmail.com>
2024-03-24 16:03:52 +0100

    fix sleeping times

--- startup_nomadiot_001.sh
+++ startup_nomadiot_001.sh
@@ -7,13 +7,13 @@ service bluetooth restart
 # Start the Bluetooth agent and configure Bluetooth settings
 echo "Starting Bluetooth agent..."
 echo "Configuring Bluetooth settings..."
-bluetoothctl system-alias nomadiot_001 & sleep 10
+bluetoothctl system-alias nomadiot_001 & sleep 5
 echo "Powering off Bluetooth..."
-bluetoothctl power off & sleep 10
+bluetoothctl power off & sleep 5
 echo "Powering on Bluetooth..."
-bluetoothctl power on & sleep 10
+bluetoothctl power on & sleep 5
 echo "Setting device to discoverable..."
-bluetoothctl discoverable on & sleep 10
+bluetoothctl discoverable on & sleep 5
 # Navigate to the specified directory
 cd /root/servocontrol-N00
 
