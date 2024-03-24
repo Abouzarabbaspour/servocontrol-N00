3fe69b58aa25f19dcd7f7d5582475f70c57779cc
nomad9898 <abouzarabbaspour@gmail.com>
2024-03-24 15:22:00 +0100

    fix script

--- startup_nomadiot_001.sh
+++ startup_nomadiot_001.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-# change directory to the location of the script
-cd /home/servocontrol-N00
-git pull
+# Change directory to the location of the script
+cd "$(dirname "$0")"
 
-# Start the bluetooth agent
+# Start the Bluetooth agent and configure Bluetooth settings
 bluetoothctl system-alias nomadiot_001
 bluetoothctl power off
 bluetoothctl power on
 bluetoothctl discoverable on
+
+# Navigate to the specified directory
+cd /home/servocontrol-N00
+
+# Pull the latest changes from Git repository
+git pull
+
+# Start the Bluetooth agent with specified capabilities and PIN
 bt-agent --capability=NoInputNoOutput -p pins &
 
 # Start the servo control script
